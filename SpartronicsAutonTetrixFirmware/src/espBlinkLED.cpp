d0b1727928ea2a149a06e8f8916566efc73475fb
loqoman <darwin.s.clark@gmail.com>
2020-11-12 20:16:36 -0800

    :art: Adding a basic AP snippippit, also first upload to ESP finished

--- SpartronicsAutonTetrixFirmware/src/espBlinkLED.cpp
+++ SpartronicsAutonTetrixFirmware/src/espBlinkLED.cpp
@@ -1,6 +1,7 @@
 #ifdef ESP82
 // From the RobotDYN docs, the 'test esp' sketch
 #include <Arduino.h> 
+#include <ESP8266WiFi.h>
 // I'm not really interested in getting the teest esp sketch to work...
 /*
 void setup() { 
@@ -33,10 +34,24 @@ void loop() {
         }    
 }
 */
+const char *ssid = "RobotWebServer"; // The name of the Wi-Fi network that will be created
+const char *password = "";   // The password required to connect to it, leave blank for an open network
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(14,OUTPUT);
+
+  Serial.begin(115200);
+  delay(4000);
+  Serial.println('\n');
+
+  WiFi.softAP(ssid, password);             // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started");
+
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.softAPIP());         // Send the IP address of the ESP8266 to the computer
 }
 
 void loop() {
